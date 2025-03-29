4d543286b3304f7ab462ded2dfc6434f10a2eef9
amarthya <amarthyaseshu2@gmail.com>
2025-03-29 23:57:56 +0530

    oauth

---
+++ grpc-stock-trading-server-main/src/main/java/com/javatechie/service/StockTradingServiceImpl.java
@@ -0,0 +1,41 @@
+package com.javatechie.service;
+
+
+import com.javatechie.entity.Stock;
+import com.javatechie.grpc.StockRequest;
+import com.javatechie.grpc.StockResponse;
+import com.javatechie.grpc.StockTradingServiceGrpc;
+import com.javatechie.repository.StockRepository;
+import io.grpc.stub.StreamObserver;
+import org.springframework.grpc.server.service.GrpcService;
+
+@GrpcService
+public class StockTradingServiceImpl extends StockTradingServiceGrpc.StockTradingServiceImplBase {
+
+
+    private final StockRepository stockRepository;
+
+    public StockTradingServiceImpl(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Override
+    public void getStockPrice(StockRequest request,
+                              StreamObserver<StockResponse> responseObserver) {
+
+        //stockName -> DB -> map response -> return
+
+        String stockSymbol = request.getStockSymbol();
+        Stock stockEntity = stockRepository.findByStockSymbol(stockSymbol);
+
+        StockResponse stockResponse = StockResponse.newBuilder()
+                .setStockSymbol(stockEntity.getStockSymbol())
+                .setPrice(stockEntity.getPrice())
+                .setTimestamp(stockEntity.getLastUpdated().toString())
+                .build();
+
+        responseObserver.onNext(stockResponse);
+        responseObserver.onCompleted();
+
+    }
+}
